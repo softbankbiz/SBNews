b75757379c2d7bb0a42cc638c3098a53fcfcdf72
softbankbiz <softbankbiz@gmail.com>
2018-09-26 16:59:01 +0900

    v0.9.6.1

--- access_counter_url.php
+++ access_counter_url.php
@@ -11,7 +11,7 @@ if ($_GET["company_id"] && $_GET["news_id"] && $_GET["issue"]) {
 	$result = $stmt->execute();
 	if ($result) {
 		$file = './images/counter.gif';
-		header("Cache-Control: private");
+		header("Cache-Control: private, must-revalidate, max-age=28800");
 		header('Content-Type: image/gif');
 		readfile($file);
 	} else {
--- redirect_url.php
+++ redirect_url.php
@@ -9,10 +9,10 @@ if (!$mysqli) {
 	die();
 }
 
-if ($_GET["company_id"] && $_GET["news_id"] && $_GET["url"] && $_GET["issue"]) {
-	$query = "INSERT INTO click_counter (company_id, news_id, url, issue) VALUES (?,?,?,?)";
+if ($_GET["company_id"] && $_GET["news_id"] && $_GET["url"] && $_GET["issue"] && $_GET["title"]) {
+	$query = "INSERT INTO click_counter (company_id, news_id, url, issue, title) VALUES (?,?,?,?,?)";
 	$stmt = $mysqli->prepare($query);
-	$stmt->bind_param("ssss", $_GET["company_id"], $_GET["news_id"], $_GET["url"], $_GET["issue"]);
+	$stmt->bind_param("sssss", $_GET["company_id"], $_GET["news_id"], $_GET["url"], $_GET["issue"], $_GET["title"]);
 	$result = $stmt->execute();
 	if ($result) {
 		header('Location: ' . $_GET["url"], true, 301);
